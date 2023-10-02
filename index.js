ab54a5a20e6edbcb374796b62cf2a1535df75970
RahmaWijaya <117076431+RahmaWijaya@users.noreply.github.com>
2023-10-02 20:33:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const txtElement=['Student',''of','SMKN 4 Bogor'];
+let count =0;
+let txtIndex=0;
+let currenTxt='';
+let words='';
+
+(function ngetik (){
+    if (count== txtElement.length){
+        count =0;
+    }}
+)
